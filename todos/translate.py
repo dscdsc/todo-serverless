f4236c0c65edd163a0e5f025db8218197efea706
EC2 Default User <ec2-user@ip-172-31-25-80.ec2.internal>
2021-01-05 11:06:44 +0000

    New Feature

---
+++ todos/translate.py
@@ -0,0 +1,37 @@
+import os
+import json
+
+from todos import decimalencoder
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+comprehend = boto3.client('comprehend')
+translate = boto3.client('translate')
+
+def get_translate(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    response_lang = comprehend.detect_dominant_language(Text=result['Item']['text'])
+    
+    slang = response_lang['Languages']
+    
+    result_translate = translate.translate_text(Text=result['Item']['text'], 
+            SourceLanguageCode=lang, TargetLanguageCode=event['pathParameters']['language'])
+    
+    result['Item']['text'] = result_translate.get('TranslatedText')
+    
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Item'],
+                           cls=decimalencoder.DecimalEncoder)
+    }
+    
+    return response
